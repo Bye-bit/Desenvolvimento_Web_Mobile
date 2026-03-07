b2b6427f917b00b045d9768d86fc7bcf8303ddeb
Lucas Fernandes <lucas.00000852836@unicap.br>
2026-03-07 22:37:44 +0000

    mudei o alt xd

--- mini_bio/app/page.js
+++ mini_bio/app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <main style={{display : "flex" , justifyContent: "center", alignItems: "center", height: '100vh'}}>
       <div className="w-150 h-200 bg-[#A6A6A6] rounded-xl shadow-xl flex flex-col items-center justify-top pt-6 outline outline-2 outline-black" >
-        <Image src = "/assets/eu.jpeg" width={120}height={120} alt = "foto do maior merda" className="rounded-full outline outline-2 outline-black"/>
+        <Image src = "/assets/eu.jpeg" width={120}height={120} alt = "foto do GOAT" className="rounded-full outline outline-2 outline-black"/>
 
          <p className="font-bold text-black text-2xl mt-4 text-center" >
           Lucas Fernandes
